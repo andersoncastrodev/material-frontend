50404ae646be3e312e20b0eed456731aa4f9c826
Anderson Castro <andersoncastrosantos@gmail.com>
2024-07-07 13:52:16 -0300

    .

--- src/shared/components/ferramentas-de-detalhes/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhes/FerramentasDeDetalhe.tsx
@@ -126,6 +126,13 @@ export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
                 </Button>        
             )}
 
+
+            { (mostrarBotaoSalvarEFecharCarregando && !mdDown && !smDown) && (
+
+                <Skeleton width={180} height={60} />
+
+            )}
+
             
             { (mostrarBotaoApagar && !mostrarBotaoApagarCarregando ) && (
 
