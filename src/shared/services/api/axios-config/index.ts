2de7d10066ca382a9150c6d4c7ea644416a37ac0
Anderson Castro <andersoncastrosantos@gmail.com>
2024-07-17 09:20:32 -0300

    ajuste em axios

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -1,2 +1,18 @@
 import axios from "axios";
+import { reponseInterceptor } from "./interceptors/ReponseInterceptor";
+import { errorInterceptor } from "./interceptors/ErrorInterceptor";
 
+const Api = axios.create({
+
+    baseURL: 'htpp://localhost:3333'
+});
+
+
+Api.interceptors.response.use(
+
+    (response) => reponseInterceptor(response),
+
+    (error) => errorInterceptor(error)
+)
+
+export { Api }
